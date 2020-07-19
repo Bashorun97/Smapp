a2595337043bc31518ad620197393d166dd70eb0
Bashorun97 <bashorun.emma@gmail.com>
2020-07-20 00:20:53 +0100

    Working models

--- app/models.py
+++ app/models.py
@@ -14,6 +14,7 @@ class Student(db.Model):
     department = db.Column(db.String(100))
     course = db.Column(db.String(100))
     level = db.Column(db.Integer)
+    courses_offered = db.Column(db.String(300))
     nationality = db.Column(db.String(20))
     next_of_kin = db.Column(db.String(30))
     next_of_kin_phone = db.Column(db.Integer)
@@ -27,27 +28,26 @@ class Course(db.Model):
     course_name  = db.Column(db.String(50))
     status = db.Column(db.String(10))
     unit = db.Column(db.Integer)
-    #dep_id = db.Column(db.Integer, db.ForeignKey('department.id'))
+    level = db.Column(db.Integer)
+    dep_id = db.Column(db.Integer, db.ForeignKey('department.id'))
     
-
     @staticmethod
     def insert_courses():
         
         courses = {
-        'MSM 111 Introduction to Marine Science':('C', 3),
-        'MSM 211 Basic Oceanography':('C', 3),
-        'MSM 520 Fish Preservation':('E', 2),
-        'SSG 312 Control Theory':('C', 2),
-        'CSC 202 Datastructures':('E', 3),
-        'EEG 201 Signals and Systems':('C', 2),
-        'SSG 512 Expert Systems':('C', 3),
-        'EEG 306 Cicrcuit Theory':('C', 2)
+        'MSM 111 Introduction to Marine Science':('C', 3, 100, 'Fisheries'),
+        'MSM 211 Basic Oceanography':('C', 3, 200, 'Fisheries'),
+        'MSM 520 Fish Preservation':('E', 2, 500, 'Fisheries'),
+        'SSG 312 Control Theory':('C', 2, 300, 'Systems Engineering'),
+        'CSC 202 Datastructures':('E', 3, 200, 'Computer Science'),
+        'EEG 201 Signals and Systems':('C', 2, 200, 'Electrical Engineering'),
+        'SSG 512 Expert Systems':('C', 3, 500, 'Systems Engineering'),
+        'EEG 306 Circuit Theory':('C', 2, 200, 'Electrical Engineering')
         }
-
-        for k,v in courses:
+        for k in courses:
             course = Course.query.filter_by(course_name=k).first()
             if course is None:
-                course = Course(course_name=k, status=v[0], unit=v[1])
+                course = Course(course_name=k, status=courses[k][0], unit=courses[k][1], level=courses[k][2], dep=Department.query.filter_by(department_name=courses[k][3]).first())
             db.session.add(course) 
         db.session.commit()
 
@@ -57,15 +57,15 @@ class Department(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     department_name = db.Column(db.String(50))
     students = db.relationship('Student', backref='dep')
-    #courses = db.Column('Course', backref=)
+    courses = db.relationship('Course', backref='dep')
 
     @staticmethod
     def insert_departments():
-        DEPARTMENTS = [
+        departments = [
         'Fisheries', 'Electrical Engineering', 'Systems Engineering',
         'Computer Science'
         ]
-        for dep in DEPARTMENTS:
+        for dep in departments:
             department = Department.query.filter_by(department_name=dep).first()
             if department is None:
                 department = Department(department_name=dep)
